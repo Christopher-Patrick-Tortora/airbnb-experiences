c29c3465a44e66e6021f40b62749a3b33c3522c2
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-01-19 23:30:33 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import  { Navbar }  from './components/Navbar.js'
 import { Hero } from './components/Hero.js';
 import { Card } from './components/Card.js';
+import bigData from './data.js'
 
 function App() {
+  const cards = bigData.map(data => {
+    return <Card
+              key={data.id}
+              {...data}
+            />
+})
+
   return (
     <div className='app'>
       <Navbar />
       <Hero />
-      <Card />
+      <div className='cards-list'>
+        {cards}
+      </div>
+      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,26 @@
 import  star  from '../images/star.png'
 import  katie  from '../images/katie-zaferes.png'
 
-const Card = () => {
+const Card = (props) => {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className='card'>
-            <img src={katie} className='card-image'></img>
+            { badgeText !== undefined && <div className='card-badge'>{badgeText}</div>}
+            <img src={katie} alt="katie" className='card-image'></img>
             <div className='card-stats'>
                 <span><img src={star} className='card-star'></img></span>
-                <span>5.0</span>
-                <span>(6) • USA</span>
+                <span>{props.stats.rating}</span>
+                <span>({props.stats.reviewCount}) • </span>
+                <span>{props.location}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p className='card-end'><b>From $136</b> / person</p>  
+            <p>{props.title}</p>
+            <p className='card-end'><b>From ${props.price}</b> / person</p>  
         </div>
   
     )
